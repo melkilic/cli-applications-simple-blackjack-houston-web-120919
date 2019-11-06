0ce614a2937dd55aa6e091f8e529f3e2dc58b58f
Melike KILIC <melike180400@gmail.com>
2019-11-06 02:51:44 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,13 +1,14 @@
 def welcome
-  # code #welcome here
+puts  "Welcome to the Blackjack Table"
 end
 
 def deal_card
-random_num= random(1..11)
+random_num= rand(1..11)
+
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+   puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
